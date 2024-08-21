38827ba5544c4fe214458c19a5e25a91ea71819f
Glenn Nelson <glenn@hexcoder.us>
2024-08-21 18:02:53 -0400

    Update Package for Meteor 3, 67726e as Owner

--- package.js
+++ package.js
@@ -1,20 +1,20 @@
 Package.describe({
-	name: 'simplesigner:jobs',
-	version: '0.0.1',
+	name: '67726e:jobs',
+	version: '3.0.0-beta-1',
 	documentation: 'README.md',
 	git: 'https://github.com/67726e/meteor.jobs',
 	summary: 'Schedule jobs to run at a later time, including multi-server, super efficient',
 });
 
 Package.onUse(function(api) {
-	api.versionsFrom('1.3');
+	api.versionsFrom(['3.0.1']);
 
 	api.use([
 		'check',
 		'ecmascript',
 		'mongo',
 		'random',
-		'typescript@3.0.0 || 4.0.0',
+		'typescript@3.0.0 || 4.0.0 || 5.0.0',
 	], 'server');
 	
 	api.export([
